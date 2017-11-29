dadcdb99660bbea4c2f065530e0c8c801af28594
Jakub Jablonski <kuba.jablonski.zg@gmail.com>
2017-11-29 22:12:34 +0100

    fetch weather data

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,27 @@
 </template>
 
 <script>
+import axios from 'axios'
 import Header from './components/Header'
+
+// 3f74814c693ce35c3c3f355cdc86db36
 
 export default {
   components: {
     myHeader: Header
+  },
+  created () {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition(position => {
+        console.log(position.coords.latitude, position.coords.longitude)
+        axios.get(`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/d459dbf7e251f54f0119998983062aa8/${position.coords.latitude},${position.coords.longitude}`)
+          .then(res => {
+            console.log(res)
+          })
+      })
+    } else {
+      alert('Poop')
+    }
   }
 }
 </script>

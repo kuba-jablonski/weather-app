28d3dc9e96b248ff030eddfe83d9c596f04d3fae
Jakub Jablonski <kuba.jablonski.zg@gmail.com>
2017-12-08 12:00:48 +0100

    style info footer

--- src/components/InfoFooter.vue
+++ src/components/InfoFooter.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="info">
-    <button @click="switchDegrees" class="info__btn">{{ btnText }}</button>
-    <p class="info__author">Created by bob durting the 100 Days of Code challenge.</p>
+    <p class="info__degrees">Weather displaying in {{ celsius ? 'Celsius' : 'Farenheit'}}.
+      <a class="info__link" @click="switchDegrees">Switch to {{ celsius ? 'Farenheit' : 'Celsius'}}.</a>
+    </p>
+    <p class="info__author">
+      Created by
+      <a class="info__link" href="https://github.com/kuba-jablonski" target="_blank">
+        Jakub Jabłoński
+      </a>
+      during the 100 Days of Code challenge.
+    </p>
   </div>
 </template>
 
@@ -35,5 +43,18 @@ export default {
 .info {
   text-align: center;
   margin-top: 2rem;
+  font-size: 1.3rem;
+
+  &__link {
+    color: $color-primary-light;
+    font-weight: 600;
+    transition: all 0.5s;
+    cursor: pointer;
+    text-decoration: none;
+
+    &:hover {
+      color: $color-primary;
+    }
+  }
 }
 </style>
